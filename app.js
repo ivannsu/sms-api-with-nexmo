94b0890a72aecccc358365dd1f996d7ab9b65df4
ivan <ivansu.webmail@gmail.com>
2018-09-01 22:43:02 +0700

    Initialize NPM, API, and Express

---
+++ app.js
@@ -0,0 +1,12 @@
+const Nexmo = require('nexmo');
+
+const nexmo = new Nexmo({
+  apiKey: 'ebe5ad00',
+  apiSecret: 'DORrSunQmtiqz2wy',
+});
+
+const from = 'Ivan.COM';
+const to = 82255517049;
+const text = `A text message from ${from}`;
+
+nexmo.message.sendSms(from, to, text);
